deb9db135e66c92ebca14b5bc7174670d9fb28bf
Bestxemchuaa <118709370+Bestxemchuaa@users.noreply.github.com>
2022-11-28 13:13:35 +0700

    Add files via upload

--- Test/waifuhub.lua
+++ Test/waifuhub.lua
@@ -735,7 +735,7 @@ function ui:W1n(text,text2,text2Pos,toclose)
 					TweenService:Create(
 						TextButton_2_Toggle2,
 						TweenInfo.new(0.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
-						{BackgroundColor3 = Color3.fromRGB(84, 0, 253)} -- UDim2.new(0, 128, 0, 25)
+						{BackgroundColor3 = Color3.fromRGB(0,0,8)} -- UDim2.new(0, 128, 0, 25)
 					):Play()
 				end)
 
@@ -5506,8 +5506,62 @@ local StopCamera = require(game.ReplicatedStorage.Util.CameraShaker)StopCamera:S
         end
     end
 end)();
+local yedkuy112 = require(game.Players.LocalPlayer.PlayerScripts.CombatFramework.CameraShaker)
+local VirtualUser = game:GetService('VirtualUser')
+local yedhee = require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework)
 
-               Main:Label("Auto Something")               
+
+spawn(function()
+    while wait() do
+        if setscriptable then
+            setscriptable(game.Players.LocalPlayer, "SimulationRadius", true)
+            game.Players.LocalPlayer.SimulationRadius = math.huge * math.huge, math.huge * math.huge * 1 / 0 * 1 / 0 * 1 / 0 * 1 / 0 * 1 / 0
+        end
+    end
+end)
+spawn(function()
+	game:GetService("RunService").Stepped:Connect(function()
+		pcall(function()
+			local yedkuy112 = require(game.Players.LocalPlayer.PlayerScripts.CombatFramework.CameraShaker)
+			local VirtualUser = game:GetService('VirtualUser')
+			local yedhee = require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework)
+            yedkuy112.CameraShakeInstance.CameraShakeState.Inactive = 0
+            yedhee.activeController.hitboxMagnitude = 55
+			if getgenv().fast then
+                
+                    if game.Players.LocalPlayer.Character:FindFirstChild("Black Leg") then
+                        yedhee.activeController.timeToNextAttack = 3
+                    else
+                        yedhee.activeController.timeToNextAttack = -(math.huge^math.huge)
+                    end
+                    yedhee.activeController.attacking = false
+                    yedhee.activeController.increment = 3
+                    if yedhee.activeController:attack() then
+                        yedhee.activeController:attack()
+                    end
+                    yedhee.activeController.blocking = false
+                    yedhee.activeController.timeToNextBlock = 0
+                    game.Players.LocalPlayer.Character.Stun.Value = 0
+                    game.Players.LocalPlayer.Character.Humanoid.Sit = false
+                    yedhee.activeController.timeToNextAttack = 0
+                    yedhee.activeController.attacking = false
+                    yedhee.activeController.blocking = false
+                    yedhee.activeController.timeToNextAttack = 0
+                    yedhee.activeController.timeToNextBlock = 0
+                    yedhee.activeController.increment = 3
+                    yedhee.activeController.hitboxMagnitude = 55
+                    yedhee.activeController.focusStart = 0
+                    if yedhee.activeController:attack() then
+                        yedhee.activeController:attack()
+                    end
+                    
+                
+			end
+		end)
+	end)
+end)
+
+              Main:Label("Auto Something")               
                    
 	Main:Toggle("Auto Chest[TP]",false,function(jiee)
 	getgenv().f = jiee
